0631446e91703d24433352c5ac3e72a880e4cb06
Leo Tomio <leo__tomio@hotmail.com>
2026-03-19 15:10:20 -0300

    config 3

--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -8,7 +8,7 @@ import Footer from "../../components/Footer";
 import Users from "../user/User";
 
 const Dashboard = () => {
-    const [activeTab, setActiveTab] = useState<string>("user");
+    const [activeTab, setActiveTab] = useState<string>("budgets");
 
     return (
         <div className="d-flex flex-column min-vh-100">
